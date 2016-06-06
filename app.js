60a675761a0dddc5bfe7d8b1d2bd0fee128dd136
Jake Bell <jakebrbell@gmail.com>
2016-06-06 15:14:12 -0700

    Add form transition

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
     $(this).text('Liked!')
       .closest('.news-item')
       .addClass('is-liked');
-  })
+  });
+
+  $('.js-add-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-form').toggleClass('is-visible');
+  });
 });
